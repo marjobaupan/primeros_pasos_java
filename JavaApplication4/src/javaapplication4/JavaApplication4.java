ff818970b1d7d004f5880b008815dccb25bdf8cc
marjobaupan <marjobaupan@gmail.com>
2014-11-16 20:13:15 -0500

    Instrucciones basicas
    For, while, do while, if

--- JavaApplication4/src/javaapplication4/JavaApplication4.java
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -14,8 +14,27 @@ public class JavaApplication4 {
     /**
      * @param args the command line arguments
      */
+    static int i;
     public static void main(String[] args) {
         // TODO code application logic here
+        for (i=0;i<7;i++){
+            System.out.println(i);
+        }
+        
+        while (i>1){
+        System.out.println(i);
+        i--;
+        }
+        
+        do {
+            System.out.println(i);
+            i++;
+        }while (i<=7);
+        
+        System.out.println(i);
+        
+        if (i==8){ System.out.print("Estas son instrucciones basicas");}
+        
     }
     
 }
